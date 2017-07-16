7cbe31ea12c561b15e87391d4423a00d43f7a959
Sumit Tada <sumittada@gmail.com>
2017-07-16 23:11:38 +0530

    Update video_to_gif.sh

--- ffmpeg/video_to_gif.sh
+++ ffmpeg/video_to_gif.sh
@@ -1,11 +1,21 @@
 # To get best results in terms of GIF quality, but slower processing and huge file sizes:
+set -e
+
+if [ ! "$#" -gt 0 ]; then
+    echo "Error: you need to specify the filename of video file to convert."
+    echo "Usage: "
+    echo "bash $0 <video_filename>"
+    exit 1
+fi
+
+filename="$1"
 
 mkdir ./pngs
 mkdir ./gifs
 
-ffmpeg -i src.mov -r 10 ./pngs/out%04d.png
+ffmpeg -i $filename -r 10 ./pngs/out%04d.png
 sips -s format gif ./pngs/*.png --out ./gifs
-gifsicle --optimize=3 --delay=3 --loopcount ./gifs/*.gif > animation.gif
+gifsicle --optimize=9 --delay=9 --loopcount ./gifs/*.gif > $filename.gif
 
 rm -rf pngs
 rm -rf gifs
